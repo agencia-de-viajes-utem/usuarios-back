369735472ab609e6f309cf78078235f46a94e6e6
lukas <lukas_skyry_14@hotmail.cl>
2023-11-24 16:59:57 -0300

    cambios 3000

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"backend/api/routes"
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	firebase "firebase.google.com/go"
 	"github.com/gorilla/mux"
@@ -13,8 +15,27 @@ import (
 
 func main() {
 	// Inicializa la configuración de Firebase
+
+	// Buscar el archivo de credenciales en el directorio actual
+	matchingPattern := "./gha-creds-*.json"
+	matches, err := filepath.Glob(matchingPattern)
+	if err != nil {
+		fmt.Printf("Error al buscar el archivo de credenciales: %v\n", err)
+
+		return
+	}
+
+	if len(matches) == 0 {
+		fmt.Println("No se encontraron archivos de credenciales.")
+
+		return
+	}
+
+	// Utilizar el primer archivo coincidente (puedes ajustar esto según tus necesidades)
+	pathToCredentials := matches[0]
+
 	ctx := context.Background()
-	opt := option.WithCredentialsFile("env-volume")
+	opt := option.WithCredentialsFile(pathToCredentials)
 	config := &firebase.Config{ProjectID: "test-5eebf"}
 	app, err := firebase.NewApp(ctx, config, opt)
 	if err != nil {
@@ -32,6 +53,6 @@ func main() {
 	// Crear un enrutador utilizando gorilla/mux
 	r := mux.NewRouter()
 	routes.ConfigureRoutes(r, app)
-
+	log.Println("ListenOn")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
